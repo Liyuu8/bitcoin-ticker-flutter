ed1780db7e25afcb17d1d446f331fd8a089c32fd
Liyuu <13977245+Liyuu8@users.noreply.github.com>
2020-06-20 20:29:58 +0900

    Implement the DropdownButton

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,11 +1,21 @@
 import 'package:flutter/material.dart';
 
+import './coin_data.dart';
+
 class PriceScreen extends StatefulWidget {
   @override
   _PriceScreenState createState() => _PriceScreenState();
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = 'USD';
+  List<DropdownMenuItem> currencyMenuItems = currenciesList
+      .map((currency) => DropdownMenuItem(
+            child: Text(currency),
+            value: currency,
+          ))
+      .toList();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +52,16 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton<String>(
+              value: selectedCurrency,
+              items: currencyMenuItems,
+              onChanged: (value) {
+                setState(() {
+                  selectedCurrency = value;
+                  print(selectedCurrency);
+                });
+              },
+            ),
           ),
         ],
       ),
